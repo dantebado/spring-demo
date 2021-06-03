a6265ed489a7b178af118b121b85b394f2e96195
Dante Mateo Bado Sucamele <me@dantebado.com>
2021-06-03 00:04:01 -0300

    get parameters

--- src/main/java/com/dantebado/springsampledds/controllers/HelloController.java
+++ src/main/java/com/dantebado/springsampledds/controllers/HelloController.java
@@ -4,14 +4,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/")
 public class HelloController {
 
     @GetMapping
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello World");
+    public ResponseEntity<String> sayHello(
+            @RequestParam(required = false, defaultValue = "World") String name
+    ) {
+        return ResponseEntity.ok("Hello " + name);
     }
 
 }
