009d1caa185e05573f20fb174fe9be49327c0b9f
Dante Mateo Bado Sucamele <me@dantebado.com>
2021-06-02 22:27:21 -0300

    initial commit

---
+++ src/test/java/com/dantebado/springsampledds/SpringSampleDdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dantebado.springsampledds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleDdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
